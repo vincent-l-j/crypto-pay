cd556a31a212a820e1b17d0ae2ca257c6db117da
Vincent Jadraque <vincent.jadraque@gmail.com>
2022-03-29 23:42:18 +1100

    Remove unused imports

--- app.py
+++ app.py
@@ -26,8 +26,6 @@
 ################################################################################
 # Imports
 import streamlit as st
-from dataclasses import dataclass
-from typing import Any, List
 from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 ################################################################################
